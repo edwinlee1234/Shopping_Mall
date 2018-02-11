431b7f94aabd54fc513774466bd87ee66220f9f9
Edwin Lee <edwinlee000@gmail.com>
2018-02-11 12:41:43 +0000

    add web routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Home
+Route::get('/', 'HomeController@index');
+
+//User
+Route::group(['prefix' => 'user'], function(){
+    Route::group(['prefix' => 'auth'], function(){
+        Route::get('/sign-up', '/User/UserController@signUpPage');
+        Route::post('/sign-up', '/User/UserController@signUpProcess');
+        Route::get('/sign-in', '/User/UserController@signInPage');
+        Route::post('/sign-in', '/User/UserController@signInProcess');
+        Route::get('/sign-out', '/User/UserController@signOut');
+    });
+});
+
+//Merchandise
+Route::group(['prefix' => 'merchandise'], function(){
+    Route::get('/', 'Merchandise/MerchandiseController@merchandiseListPage');
+    Route::get('/create', 'Merchandise/MerchandiseController@merchandiseCreateProcess');
+    Route::get('/manage', 'Merchandise/MerchandiseController@merchandiseManageListPage');
+    
+    Route::group(['prefix' => '{merchandise_id}'], function(){
+        Route::get('/', 'Merchandise/MerchandiseController@merchandiseItemPage');
+        Route::get('/edit', 'Merchandise/MerchandiseController@merchandiseItemEditPage');
+        Route::put('/', 'Merchandise/MerchandiseController@merchandiseItemUpdate');
+        Route::post('/', 'Merchandise/MerchandiseController@merchandiseBuyProcess');
+    });
+});
+
+//Order
+Route::group(['prefix' => 'order'], function(){
+    Route::get('/', 'Order/OrderController@orderListPage');
 });
