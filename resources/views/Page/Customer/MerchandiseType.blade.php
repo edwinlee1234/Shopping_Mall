63ea97a3355417a05e0a13d246bea25215254a03
Edwin Lee <edwinlee000@gmail.com>
2018-03-02 13:18:22 +0000

    add template

---
+++ resources/views/Page/Customer/MerchandiseType.blade.php
@@ -0,0 +1,15 @@
+@extends('Page/Customer/App')
+
+@section('title', $title)
+
+@section('style')
+@parent
+@endsection
+
+@section('content')
+    <h1>type</h1>
+@endsection
+
+@section('script')
+@parent
+@endsection
